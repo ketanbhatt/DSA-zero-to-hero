b549df8a41b23e7cf089b994c0a617d19ac9a63d
TigerApps <arpitgupta_15@yahoo.co.in>
2015-07-02 13:05:43 +0530

    Added merge sort to sorting.py

--- Day2/sorting.py
+++ Day2/sorting.py
@@ -11,8 +11,7 @@ import random
 # selection sort
 # time complexity: O(n^2)
 # space complexity: O(1)
-def selection(arr):
-    n = len(arr)
+def selection(arr,n):
     for i in range(n):
         small = i
         for j in range(i+1,n):
@@ -24,8 +23,7 @@ def selection(arr):
 # bubble sort
 # time complexity: O(n^2)
 # space complexity: O(1)
-def bubble(arr):
-    n = len(arr)
+def bubble(arr,n):
     for i in range(n):
         for j in range(n-1-i):
             if arr[j] > arr[j+1]:
@@ -35,8 +33,7 @@ def bubble(arr):
 # insertion sort
 # time complexity: O(n^2)
 # space complexity: O(1)
-def insertion(arr):
-    n = len(arr)
+def insertion(arr,n):
     for i in range(1,n):
         temp = i
         for j in range(i-1,-1,-1):
@@ -47,15 +44,45 @@ def insertion(arr):
     return arr
 
 # merge sort
-# time complexity:
-# space complexity:
-def merge(arr):
+# time complexity: O(nLog(n))
+# space complexity: O(n)
+def merge(arr,s,e):
+    if s == e:
+        return arr
+    m = (e-s)/2 + s #divide at midpoint
+    merge(arr,s,m)
+    merge(arr,m+1,e) #conquer the halves
+    arr = merge_h(arr,s,e)
     return arr
+
+# merge function
+def merge_h(arr,s,e):
+    m = (e-s)/2 + s
+    left  = arr[s:m+1]
+    l = 0
+    right = arr[m+1:e+1]
+    r = 0
+    for i in range(s,e+1):  
+        if l<m+1-s and r<e-m:   #while l and r in range, keep adding smalled
+            if left[l] <= right[r]:
+                arr[i] = left[l]
+                l += 1
+            else:
+                arr[i] = right[r]
+                r += 1
+        elif l>=m+1-s:      #l out of range, add right
+            arr[i] = right[r]
+            r += 1  
+        else:               #r out of range, add left
+            arr[i] = left[l]
+            l += 1
+    return arr      
+
 
 # quick sort
 # time complexity:
 # space complexity:
-def quick(arr):
+def quick(arr,s,e):
     return arr
 
 # heap sort
@@ -66,7 +93,7 @@ def heap(arr):
 
 #array ofrandom ints
 arr = []
-n = 16
+n = 15
 for i in range(n):  
     arr.append(random.randint(0,100))
 
@@ -79,10 +106,10 @@ l6 = arr[:]
 
 print "Array:           ",arr
 print "Sorted Array:    ",sorted(arr)
-print "Selection Sort:  ",selection(l1)
-print "Bubble Sort:     ",bubble(l2)
-print "Insertion Sort:  ",insertion(l3)
-print "Merge Sort:      ",merge(l4)
-print "Quick Sort:      ",quick(l5)
+print "Selection Sort:  ",selection(l1,n)
+print "Bubble Sort:     ",bubble(l2,n)
+print "Insertion Sort:  ",insertion(l3,n)
+print "Merge Sort:      ",merge(l4,0,n-1)
+print "Quick Sort:      ",quick(l5,0,n-1)
 print "Heap Sort:       ",heap(l6)
 
